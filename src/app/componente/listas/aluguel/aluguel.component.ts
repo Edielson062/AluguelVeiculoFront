8a7e3d94d8e3dd941944dd5a0388916414e94891
edielson <edielsongomes2712@gmail.com>
2025-05-19 21:35:27 -0300

    lista de Aluguel

--- src/app/componente/listas/aluguel/aluguel.component.ts
+++ src/app/componente/listas/aluguel/aluguel.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {Aluguel} from '../../../models/aluguel';
+import {AluguelService} from '../../../service/aluguel.service';
+import {NgIf} from '@angular/common';
+import {Panel} from 'primeng/panel';
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-aluguel',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    Panel,
+    TableModule
+  ],
   templateUrl: './aluguel.component.html',
   styleUrl: './aluguel.component.css'
 })
 export class AluguelComponent {
+listaAlugueis:Aluguel [] = [];
 
+
+  constructor(private service:AluguelService) {
+    this.service.listarAluguels().subscribe(aluguels => this.listaAlugueis = aluguels)
+  }
+
+  removerCliente(id:number):void{
+    this.service.deletarAlugeul(id).subscribe(() => {
+      this.listaAlugueis = this.listaAlugueis.filter(aluguel => aluguel.id !== id)
+    });
+  }
 }
